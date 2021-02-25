effcbb8e8fa0681009dd55b3a9f3c70c5135b18c
Renat <uid570@gmail.com>
2021-02-25 12:11:52 +0600

    4 commit

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -2,24 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Command;
 use Illuminate\Http\Request;
+use Telegram\Bot\Keyboard\Keyboard;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class BotController extends Controller
 {
     public function telegram()
     {
+        //dd($this->getCommand("/start"));
         $response = Telegram::getWebHookUpdates();
         if (!$response->isEmpty()) {
             $chat_id = $response['message']['chat']['id'];
             $text = $response['message']['text'];
 
-            Telegram::sendMessage([
-                'chat_id' => $chat_id,
-                'text' => $text,
-            ]);
+            if ($text == '/start')
+            {
+                $command = $this->getCommand($text);
+
+                if (!empty($command)) {
+                    $message['chat_id'] = $chat_id;
+                    $message['text'] = $command->text;
+
+                    if (!$command->keyboards->isEmpty()) {
+                        foreach ($command->keyboards as $key => $value) {
+                            $buttons[] = $value->name;
+                        }
+    
+                        if (isset($buttons)) {
+                            $keyboard = Keyboard::make([
+                                'keyboard' => $buttons,
+                                'resize_keyboard' => true,
+                                'one_time_keyboard' => true
+                            ]);
+                            $message['reply_markup'] = $keyboard;
+                        }
+                    }
+
+                    Telegram::sendMessage($message);
+                }
+            }
+            else
+            {
+                Telegram::sendMessage([
+                    'chat_id' => $chat_id,
+                    'text' => "Error",
+                ]);
+            }
         }
         
         return response('Hello User', 200)->header('Content-Type', 'text/plain');
+    }
+
+    //
+    private function getCommand($text) {
+        $command = Command::with([
+            'keyboards'
+        ])->where('name', $text)->first();
+
+        return $command;
     }
 }
