9b0a484f83d15198f65eeabfda3714ec1ac10ae6
Renat <uid570@gmail.com>
2021-02-25 12:05:08 +0600

    3 commit

--- config/telegram.php
+++ config/telegram.php
@@ -34,7 +34,7 @@ return [
     'bots'                         => [
         'mybot' => [
             'username'            => 'TelegramBot',
-            'token'               => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
+            'token'               => env('TELEGRAM_BOT_TOKEN', '1566040157:AAGoPyDpoZJdkoj1UOkBiu-aviUAWqkLi50'),
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
